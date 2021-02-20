ba5821b6c36656a1a748bf2b36b8588ff2019126
dennerpl <denner.souzapl@gmail.com>
2021-02-20 09:07:16 -0300

    Updates

---
+++ precache-manifest.3ba9425c8f8e86f50b8b05982fc2be31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9fff872e1803b5abc6250fe058f1d3",
+    "url": "/your-app-name/index.html"
+  },
+  {
+    "revision": "ff3c5090a338a8323c57",
+    "url": "/your-app-name/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9ca1c8ec87f9e416ed9b",
+    "url": "/your-app-name/static/js/2.9f9a3b49.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/your-app-name/static/js/2.9f9a3b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3c5090a338a8323c57",
+    "url": "/your-app-name/static/js/main.57ee9070.chunk.js"
+  },
+  {
+    "revision": "b67afc918c916ec5288d",
+    "url": "/your-app-name/static/js/runtime-main.7c070982.js"
+  }
+]);
